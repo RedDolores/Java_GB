a6d73dc31a9e09bb3cabc32bfbc13e33df4a7eb8
Lolita <dee12098@yandex.ru>
2023-05-25 19:45:58 +0300

    lesson2_m

--- lesson2/lesson2/src/main/java/org/example/Log.java
+++ lesson2/lesson2/src/main/java/org/example/Log.java
@@ -1,2 +1,20 @@
-package org.example;public class Log {
+package org.example;
+
+import java.io.FileInputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Log {
+    static {
+        //полный путь до файла с конфигами
+        try (FileInputStream in = new FileInputStream("log.config")) {
+            LogManager.getLogManager().readConfiguration(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger log(String className) {
+        return Logger.getLogger(className);
+    }
 }
